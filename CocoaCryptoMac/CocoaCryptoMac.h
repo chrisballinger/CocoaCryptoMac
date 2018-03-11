41a811ac339eded64e737bd3721735f30ebfc33e
Chris Ballinger <chrisballinger@gmail.com>
2018-03-11 12:58:03 -0700

    Add CocoaCryptoMac.framework target

---
+++ CocoaCryptoMac/CocoaCryptoMac.h
@@ -0,0 +1,24 @@
+//
+//  CocoaCryptoMac.h
+//  CocoaCryptoMac
+//
+//  Created by Chris Ballinger on 3/11/18.
+//  Copyright © 2018 Nik Youdale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaCryptoMac.
+FOUNDATION_EXPORT double CocoaCryptoMacVersionNumber;
+
+//! Project version string for CocoaCryptoMac.
+FOUNDATION_EXPORT const unsigned char CocoaCryptoMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaCryptoMac/PublicHeader.h>
+
+#import "CCMPublicKey.h"
+#import "CCMPrivateKey.h"
+#import "CCMKeyLoader.h"
+#import "CCMBase64.h"
+#import "CCMCryptor.h"
+#import "CSSMRSACryptor.h"
